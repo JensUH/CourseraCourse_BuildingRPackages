ddf4ccdccc9fe4426280bf7f5c37627892144222
Jens Ulrik Hansen <jensuhansen@gmail.com>
2017-01-08 22:17:14 +0100

    Adding tests

--- tests/testthat/testOfFARSFunctions.R
+++ tests/testthat/testOfFARSFunctions.R
@@ -1,5 +1,11 @@
 context("Testing FARS functions")
 
-test_that("Something is true!", {
-  expect_that(TRUE)
+test_that("make_filename", {
+  expect_equal(make_filename("2015"), "accident_2015.csv.bz2")
 })
+
+test_that("fars_read", {
+  expect_error(fars_read("sillyFile"), "file 'sillyFile' does not exist")
+})
+
+
